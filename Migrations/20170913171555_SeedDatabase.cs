cbf4b51b98f891580089c4a37fa5f01f4dede31e
ashkan-rmk <ashkan.rmk@hotmail.com>
2017-09-13 22:30:59 +0430

    SeedDatabase

---
+++ Migrations/20170913171555_SeedDatabase.cs
@@ -0,0 +1,42 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+using System;
+using System.Collections.Generic;
+
+namespace vega.Migrations
+{
+    public partial class SeedDatabase : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.Sql("INSERT INTO Makes (Name) VALUES ('Make1')");
+            migrationBuilder.Sql("INSERT INTO Makes (Name) VALUES ('Make2')");
+            migrationBuilder.Sql("INSERT INTO Makes (Name) VALUES ('Make3')");
+
+            migrationBuilder.Sql(@"INSERT INTO Models (Name, MakeId) VALUES ('Make1-ModelA', 
+            (SELECT Id FROM Makes Where Name = 'Make1'))");
+            migrationBuilder.Sql(@"INSERT INTO Models (Name, MakeId) VALUES ('Make1-ModelB', 
+            (SELECT Id FROM Makes Where Name = 'Make1'))");
+            migrationBuilder.Sql(@"INSERT INTO Models (Name, MakeId) VALUES ('Make1-ModelC', 
+            (SELECT Id FROM Makes Where Name = 'Make1'))");
+
+            migrationBuilder.Sql(@"INSERT INTO Models (Name, MakeId) VALUES ('Make2-ModelA', 
+            (SELECT Id FROM Makes Where Name = 'Make2'))");
+            migrationBuilder.Sql(@"INSERT INTO Models (Name, MakeId) VALUES ('Make2-ModelB', 
+            (SELECT Id FROM Makes Where Name = 'Make2'))");
+            migrationBuilder.Sql(@"INSERT INTO Models (Name, MakeId) VALUES ('Make2-ModelC', 
+            (SELECT Id FROM Makes Where Name = 'Make2'))");
+
+            migrationBuilder.Sql(@"INSERT INTO Models (Name, MakeId) VALUES ('Make3-ModelA', 
+            (SELECT Id FROM Makes Where Name = 'Make3'))");
+            migrationBuilder.Sql(@"INSERT INTO Models (Name, MakeId) VALUES ('Make3-ModelB', 
+            (SELECT Id FROM Makes Where Name = 'Make3'))");
+            migrationBuilder.Sql(@"INSERT INTO Models (Name, MakeId) VALUES ('Make3-ModelC', 
+            (SELECT Id FROM Makes Where Name = 'Make3'))");
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.Sql("DELETE FROM Makes where Name IN ('Make1', 'Make2', 'Make3')");
+        }
+    }
+}
--- Untitled-1.sql
+++ Untitled-1.sql
@@ -1,4 +1,3 @@
--- Get a list of tables and views in the current database
-SELECT table_catalog [database], table_schema [schema], table_name name, table_type type
-FROM information_schema.tables
+-- Select rows from a Table or View 'Makes' in schema 'SchemaName'
+SELECT * FROM Models
 GO
