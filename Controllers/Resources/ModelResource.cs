b362f1f10fbbc265cfea6f599ce09e119bfd9f0b
ashkan-rmk <ashkan.rmk@hotmail.com>
2017-09-14 02:11:48 +0430

    Build Simple Api

---
+++ Controllers/Resources/ModelResource.cs
@@ -0,0 +1,10 @@
+using vega.Models;
+
+namespace vega.Controllers.Resources
+{
+    public class ModelResource
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+    }
+}
