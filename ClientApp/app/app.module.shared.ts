3d4d716432c5849b4fee350257f2f781fc8a1e37
ashkan-rmk <ashkan.rmk@hotmail.com>
2017-09-20 20:43:51 +0430

    Add vehicle-form component

--- ClientApp/app/app.module.shared.ts
+++ ClientApp/app/app.module.shared.ts
@@ -9,6 +9,7 @@ import { NavMenuComponent } from './components/navmenu/navmenu.component';
 import { HomeComponent } from './components/home/home.component';
 import { FetchDataComponent } from './components/fetchdata/fetchdata.component';
 import { CounterComponent } from './components/counter/counter.component';
+import { VehicleFormComponent } from "./components/vehicle-form/vehicle-form.component";
 
 @NgModule({
     declarations: [
@@ -16,7 +17,8 @@ import { CounterComponent } from './components/counter/counter.component';
         NavMenuComponent,
         CounterComponent,
         FetchDataComponent,
-        HomeComponent
+        HomeComponent,
+        VehicleFormComponent
     ],
     imports: [
         CommonModule,
@@ -27,7 +29,8 @@ import { CounterComponent } from './components/counter/counter.component';
             { path: 'home', component: HomeComponent },
             { path: 'counter', component: CounterComponent },
             { path: 'fetch-data', component: FetchDataComponent },
-            { path: '**', redirectTo: 'home' }
+            { path: '**', redirectTo: 'home' },
+            { path: 'vehicles/new', component: VehicleFormComponent },
         ])
     ]
 })
