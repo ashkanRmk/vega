ff8813487edfd24d1f70a3faad7da790fecebbdf
ashkan-rmk <ashkan.rmk@hotmail.com>
2017-09-24 23:08:17 +0330

    dealing many to many realationship

--- Migrations/VegaDbContextModelSnapshot.cs
+++ Migrations/VegaDbContextModelSnapshot.cs
@@ -66,11 +66,74 @@ namespace vega.Migrations
                     b.ToTable("Models");
                 });
 
+            modelBuilder.Entity("vega.Models.Vehicle", b =>
+                {
+                    b.Property<int>("Id")
+                        .ValueGeneratedOnAdd();
+
+                    b.Property<string>("ContactEmail")
+                        .HasMaxLength(255);
+
+                    b.Property<string>("ContactName")
+                        .IsRequired()
+                        .HasMaxLength(255);
+
+                    b.Property<string>("ContactPhone")
+                        .IsRequired()
+                        .HasMaxLength(255);
+
+                    b.Property<bool>("IsRegistered");
+
+                    b.Property<DateTime>("LastUpdate");
+
+                    b.Property<int>("ModelId");
+
+                    b.HasKey("Id");
+
+                    b.HasIndex("ModelId");
+
+                    b.ToTable("Vehicles");
+                });
+
+            modelBuilder.Entity("vega.Models.VehicleFeature", b =>
+                {
+                    b.Property<int>("VehicleId");
+
+                    b.Property<int>("FeatureId");
+
+                    b.HasKey("VehicleId", "FeatureId");
+
+                    b.HasIndex("FeatureId");
+
+                    b.ToTable("VehicleFeatures");
+                });
+
             modelBuilder.Entity("vega.Models.Model", b =>
                 {
                     b.HasOne("vega.Models.Make", "Make")
                         .WithMany("Models")
                         .HasForeignKey("MakeId")
+                        .OnDelete(DeleteBehavior.Cascade);
+                });
+
+            modelBuilder.Entity("vega.Models.Vehicle", b =>
+                {
+                    b.HasOne("vega.Models.Model", "Model")
+                        .WithMany()
+                        .HasForeignKey("ModelId")
+                        .OnDelete(DeleteBehavior.Cascade);
+                });
+
+            modelBuilder.Entity("vega.Models.VehicleFeature", b =>
+                {
+                    b.HasOne("vega.Models.Feature", "Feature")
+                        .WithMany()
+                        .HasForeignKey("FeatureId")
+                        .OnDelete(DeleteBehavior.Cascade);
+
+                    b.HasOne("vega.Models.Vehicle", "Vehicle")
+                        .WithMany("Features")
+                        .HasForeignKey("VehicleId")
                         .OnDelete(DeleteBehavior.Cascade);
                 });
 #pragma warning restore 612, 618
