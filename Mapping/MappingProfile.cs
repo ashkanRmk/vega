9273bbdacafbdbcc999ee5db5eeee1393e4a73d7
ashkan-rmk <ashkan.rmk@hotmail.com>
2017-09-26 18:33:19 +0330

    adding record to DB

--- Mapping/MappingProfile.cs
+++ Mapping/MappingProfile.cs
@@ -13,6 +13,9 @@ namespace vega.Mapping
             CreateMap<Make, MakeResource>();
             CreateMap<Model, ModelResource>();
             CreateMap<Feature, FeatureResource>();
+            CreateMap<Vehicle, VehicleResource>()
+                .ForMember(vr => vr.Contact, opt => opt.MapFrom(v => new ContactResource{ Name = v.ContactName, Phone = v.ContactPhone, Email = v.ContactEmail}))
+                .ForMember(vr => vr.Features, opt => opt.MapFrom(v => v.Features.Select(vf => vf.FeatureId)));
 
             //API Resource to Domain Class 
             CreateMap<VehicleResource, Vehicle>()
