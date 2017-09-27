c2d73caf6566deb71228ccfd0c3cd26a4935e7c6
ashkan-rmk <ashkan.rmk@hotmail.com>
2017-09-27 19:53:25 +0330

    refactor using LINQ

--- Mapping/MappingProfile.cs
+++ Mapping/MappingProfile.cs
@@ -27,17 +27,15 @@ namespace vega.Mapping
                 .ForMember(v => v.Features, opt => opt.Ignore())
                 .AfterMap((vr, v) => {
                     //Remove Unselected Features
-                    var removedFeatures = new List<VehicleFeature>();
-                    foreach (var f in v.Features)
-                        if (!vr.Features.Contains(f.FeatureId))
-                            removedFeatures.Add(f);
+                    var removedFeatures = v.Features.Where(f => !vr.Features.Contains(f.FeatureId));
                     foreach (var f in removedFeatures)
                         v.Features.Remove(f);
                     
                     //Add new Features
-                    foreach (var id in vr.Features)
-                        if (!v.Features.Any(f => f.FeatureId == id))
-                            v.Features.Add(new VehicleFeature { FeatureId = id});
+                    var addedFeatures = vr.Features.Where(id => !v.Features.Any(f => f.FeatureId == id))
+                        .Select(id => new VehicleFeature{FeatureId = id});
+                    foreach (var f in addedFeatures)
+                        v.Features.Add(f);
 
                 });
         }
