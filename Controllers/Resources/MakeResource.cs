19c5068b868168b65ca0dfa199fb50897f76c752
ashkan-rmk <ashkan.rmk@hotmail.com>
2017-09-27 20:48:29 +0330

    Refactoring Using KeyValuePairResource

--- Controllers/Resources/MakeResource.cs
+++ Controllers/Resources/MakeResource.cs
@@ -4,15 +4,13 @@ using vega.Models;
 
 namespace vega.Controllers.Resources
 {
-    public class MakeResource
+    public class MakeResource : KeyValuePairResource
     {
-        public int Id { get; set; }
-        public string Name { get; set; }
-        public ICollection<ModelResource> Models { get; set; }
+        public ICollection<KeyValuePairResource> Models { get; set; }
 
         public MakeResource()
         {
-            Models = new Collection<ModelResource>();
+            Models = new Collection<KeyValuePairResource>();
         }   
     }
 }
--- Mapping/MappingProfile.cs
+++ Mapping/MappingProfile.cs
@@ -12,15 +12,16 @@ namespace vega.Mapping
         {
             //Domain Class to API Resource
             CreateMap<Make, MakeResource>();
-            CreateMap<Model, ModelResource>();
-            CreateMap<Feature, FeatureResource>();
+            CreateMap<Make, KeyValuePairResource>();
+            CreateMap<Model, KeyValuePairResource>();
+            CreateMap<Feature, KeyValuePairResource>();
             CreateMap<Vehicle, SaveVehicleResource>()
                 .ForMember(vr => vr.Contact, opt => opt.MapFrom(v => new ContactResource{ Name = v.ContactName, Phone = v.ContactPhone, Email = v.ContactEmail}))
                 .ForMember(vr => vr.Features, opt => opt.MapFrom(v => v.Features.Select(vf => vf.FeatureId)));
             CreateMap<Vehicle, VehicleResource>()
                 .ForMember(vr => vr.Make, opt => opt.MapFrom(v => v.Model.Make))
                 .ForMember(vr => vr.Contact, opt => opt.MapFrom(v => new ContactResource{ Name = v.ContactName, Phone = v.ContactPhone, Email = v.ContactEmail}))
-                .ForMember(vr => vr.Features, opt => opt.MapFrom(v => v.Features.Select(vf => new FeatureResource{ Id = vf.Feature.Id, Name = vf.Feature.Name})));
+                .ForMember(vr => vr.Features, opt => opt.MapFrom(v => v.Features.Select(vf => new KeyValuePairResource{ Id = vf.Feature.Id, Name = vf.Feature.Name})));
             
 
             //API Resource to Domain Class 
