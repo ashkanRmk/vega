73bd4bac6c3eb82bcccc643c6c812914755981ca
ashkan-rmk <ashkan.rmk@hotmail.com>
2017-10-10 19:18:15 +0330

    Refactoring sorting logic using dictionaries

--- Persistence/VehicleRepository.cs
+++ Persistence/VehicleRepository.cs
@@ -4,6 +4,8 @@ using vega.Core.Models;
 using vega.Core;
 using System.Collections.Generic;
 using System.Linq;
+using System.Linq.Expressions;
+using System;
 
 namespace vega.Persistence
 {
@@ -53,17 +55,17 @@ namespace vega.Persistence
             if (queryObj.ModelId.HasValue)
                 query = query.Where(v => v.ModelId == queryObj.ModelId.Value);
 
-            if (queryObj.SortBy == "make")
-                query = (queryObj.IsSortAscending) ? query.OrderBy(v => v.Model.Make.Name) : query.OrderByDescending(v => v.Model.Make.Name);
+            var columnsMap = new Dictionary<string, Expression<Func<Vehicle, object>>>(){
+                ["make"] = v => v.Model.Make.Name,
+                ["model"] = v => v.Model.Name,
+                ["contactName"] = v => v.ContactName,
+                ["id"] = v => v.Id
+            };
 
-            if (queryObj.SortBy == "model")
-                query = (queryObj.IsSortAscending) ? query.OrderBy(v => v.Model.Name) : query.OrderByDescending(v => v.Model.Name);
-
-            if (queryObj.SortBy == "contactName")
-                query = (queryObj.IsSortAscending) ? query.OrderBy(v => v.ContactName) : query.OrderByDescending(v => v.ContactName);
-
-            if (queryObj.SortBy == "id")
-                query = (queryObj.IsSortAscending) ? query.OrderBy(v => v.Id) : query.OrderByDescending(v => v.Id);
+            if (queryObj.IsSortAscending)
+                query = query.OrderBy(columnsMap[queryObj.SortBy]);
+            else
+                query = query.OrderByDescending(columnsMap[queryObj.SortBy]);
 
             return await query.ToListAsync();
         }
