2af79b0c794daaa4aac989be17678850fd4cae00
ashkan-rmk <ashkan.rmk@hotmail.com>
2017-10-10 19:02:42 +0330

    Sorting on the server

--- Controllers/Resources/VehicleQueryResource.cs
+++ Controllers/Resources/VehicleQueryResource.cs
@@ -4,5 +4,7 @@ namespace vega.Controllers.Resources
     {
         public int? MakeId { get; set; }
         public int? ModelId { get; set; }
+        public string SortBy { get; set; }
+        public bool IsSortAscending { get; set; }
     }
 }
